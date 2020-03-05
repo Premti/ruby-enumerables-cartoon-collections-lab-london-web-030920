1165e006db676cd823679d4056b37e31da1a4880
premti <premti@gmail.com>
2020-03-05 12:39:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,10 +22,15 @@ end
 def find_the_cheese(contains_cheese)
   new_array = []
   cheese_types = ["cheddar", "gouda", "camembert"]
+<<<<<<< HEAD
    contains_cheese.each do |array_element|
      if cheese_types.include?(array_element)
        new_array.push(array_element)
      end
+=======
+   cheese_types.each do |contains_cheese|
+     new_array.push(cheese_types.include?(contains_cheese))
+>>>>>>> a48153b7ca81a1581937320e15f8b32e1f1315a4
    end
    new_array[0]
 end
